d813472282c915573062ed4d2da25600dac3b13c
Dave Walsh <iandidave@yahoo.com>
2012-03-26 11:54:44 -0400

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,16 @@ class User < ActiveRecord::Base
 	#Tell rails which attributes can be modified automatically by outside users. Only the given attributes—are automatically accessible to outside users.
 	attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+	before_save :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
   valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true, format: { with: valid_email_regex }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6}
+	
+	private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
